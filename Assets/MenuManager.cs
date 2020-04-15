fbd342c4d7309e1f40ee0532d719138e812e00c4
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-04-15 14:34:41 -0600

    GetPictureButton
    now it needs to grab from the phone camera

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public RawImage targetPicture;
+    public Texture grabbedTexture;
+
+    public void GetPicture()
+    {
+        targetPicture.texture = grabbedTexture;
+    }
+}
