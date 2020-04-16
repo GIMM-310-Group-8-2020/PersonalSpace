bd39f123c6c37d9b99ba4018e7b8609337a6c719
Ethan Mathes <ethan@bfitec.com>
2020-04-15 18:04:25 -0600

    Got gallery pull working

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -6,10 +6,26 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     public RawImage targetPicture;
-    public Texture grabbedTexture;
 
     public void GetPicture()
     {
-        targetPicture.texture = grabbedTexture;
-    }
+		NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+		{
+			Debug.Log("Image path: " + path);
+			if (path != null)
+			{
+				// Create Texture from selected image
+				Texture2D texture = NativeGallery.LoadImageAtPath(path, 1028);
+				if (texture == null)
+				{
+					Debug.Log("Couldn't load texture from " + path);
+					return;
+				}
+
+				targetPicture.texture = texture;
+			}
+		}, "Select a PNG image", "image/png");
+
+		Debug.Log("Permission result: " + permission);
+	}
 }
