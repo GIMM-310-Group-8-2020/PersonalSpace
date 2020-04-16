e3e4b250596cf425ed314f05a971805312dd70f9
hanwiles <54829099+hanwiles@users.noreply.github.com>
2020-04-16 13:47:41 -0600

    Picture taking
    Got the access to the camera working, you can take a picture, now we just need to make a place to save it.

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -30,4 +30,67 @@ public class MenuManager : MonoBehaviour
 
         Debug.Log("Hello");
 	}
+
+    public void TakePicture(int maxSize)
+    {
+        NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                // Create a Texture2D from the captured image
+                Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+
+                // Assign texture to a temporary quad and destroy it after 5 seconds
+                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                quad.transform.forward = Camera.main.transform.forward;
+                quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+                Material material = quad.GetComponent<Renderer>().material;
+                if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+                    material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+                material.mainTexture = texture;
+
+                Destroy(quad, 5f);
+
+                // If a procedural texture is not destroyed manually, 
+                // it will only be freed after a scene change
+                Destroy(texture, 5f);
+            }
+        }, maxSize);
+
+        Debug.Log("Permission result: " + permission + ", Taking Photo");
+    }
+
+    /*public void Screenshot()
+    {
+        Debug.Log("Are you taking a screenshot?");
+
+       // NativeGallery.SaveImageToGallery(byte[] mediaBytes, string album, string filename, MediaSaveCallback callback = null);
+
+        StartCoroutine(TakeScreenshotAndSave());
+    }
+
+    private IEnumerator TakeScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        // Save the screenshot to Gallery/Photos
+        Debug.Log("Permission result: " + NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png"));
+
+        // To avoid memory leaks
+        Destroy(ss);
+    }
+    */
 }
