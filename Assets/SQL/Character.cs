a095af7cb9add12915ffca39b8eae4a12fe7c800
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-04-29 23:22:12 -0600

    CharacterDatabase
    created database classes for characters

---
+++ Assets/SQL/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataBank
+{
+    public class CharacterData
+    {
+        public string _id;
+        public string _type;
+        public GameObject _character;
+        public string _dateCreated;
+
+        public CharacterData(string id, string type, GameObject character)
+        {
+            _id = id;
+            _type = type;
+            _character = character;
+            _dateCreated = "";
+        }
+
+        public CharacterData(string id, string type, GameObject character, string dateCreated)
+        {
+            _id = id;
+            _type = type;
+            _character = character;
+            _dateCreated = dateCreated;
+        }
+    }
+}
+
